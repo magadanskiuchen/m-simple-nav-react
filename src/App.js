35440cc47644ee98fbd3671623d87ae68481a7fa
georgi <georgi@htmlbox.net>
2021-03-18 14:17:00 +0200

    Add React Router and set-up basic App structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <header className="header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Address</Link>
+              </li>
+              <li>
+                <Link to="/lat-lng">Lat/Lng</Link>
+              </li>
+              <li>
+                <Link to="/favorites">Favorites</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        
+        <div className="destination-input">
+          <Switch>
+            <Route path="/">
+              // show address input
+            </Route>
+            <Route path="/lat-lng">
+              // show lat/lng input
+            </Route>
+            <Route path="/favorites">
+              // show favorites list
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
