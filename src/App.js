3e131164c41c2ee29add576f386233583db0416b
georgi <georgi@htmlbox.net>
2021-03-22 12:36:50 +0200

    Switch to SASS/SCSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import LocationInput from './components/LocationInput';
 import FavoritesInput from './components/FavoritesInput';
 
-import './styles/App.css';
+import './styles/App.scss';
 
 function App() {
 	return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import '../styles/Header.css';
+import '../styles/Header.scss';
 
 export default function Header(props) {
 	return <header className="header">
 		<nav>
 			<ul>
-				{props.links.map(l => <Link key={l.url} to={l.url}>{l.label}</Link> )}
+				{props.links.map(l => <li><Link key={l.url} to={l.url}>{l.label}</Link></li> )}
 			</ul>
 		</nav>
 	</header>;
