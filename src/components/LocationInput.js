2bffc54367093f17671942e9a23954e4e00980a7
georgi <georgi@htmlbox.net>
2021-03-22 10:50:49 +0200

    Refactor components to functional set-up

--- src/components/LocationInput.js
+++ src/components/LocationInput.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import classnames from 'classnames';
 
-class LocationInput extends React.Component {
-	render() {
-		return (
-			<form className={classnames('location-input', this.props.className)}>
-				<div className="location-input__fields">
-					{this.props.children}
-				</div>
-				
-				<footer className="location-input__footer">
-					<input type="submit" value="Search" />
-				</footer>
-			</form>
-		);
-	}
+export default function LocationInput(props) {
+	return <form className={classnames('location-input', props.className)}>
+		<div className="location-input__fields">
+			{props.children}
+		</div>
+		
+		<footer className="location-input__footer">
+			<input type="submit" value="Search" />
+		</footer>
+	</form>;
 }
-
-export default LocationInput;
