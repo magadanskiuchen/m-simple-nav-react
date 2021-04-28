d622eb17fe28585af43e049f4bd6c5dbd667cf08
georgi <georgi@htmlbox.net>
2021-04-28 15:11:24 +0300

    Add support for deleting a favorite item

--- src/components/FavoritesList.js
+++ src/components/FavoritesList.js
@@ -5,8 +5,8 @@ import db from '../lib/db';
 import '../styles/FavoritesList.scss';
 
 export default function FavoritesList(props) {
-	const editFavorite = async (e) => {
-		const oldName = e.target.parentElement.querySelector('.saved-place__label').textContent
+	const editFavorite = (e) => {
+		const oldName = e.target.parentElement.querySelector('.saved-place__label').textContent;
 		const newName = prompt("New Name", oldName);
 		
 		if (newName !== oldName) {
@@ -14,11 +14,19 @@ export default function FavoritesList(props) {
 		}
 	};
 	
+	const deleteFavorite = (e) => {
+		const name = e.target.parentElement.querySelector('.saved-place__label').textContent;
+		
+		if (window.confirm(`Are you sure you'd like to delete ${name}?`)) {
+			db.favorites.delete(parseInt(e.target.value));
+		}
+	};
+	
 	return <ul className="favorites-list">
 		{props.favoritePlaces.map(p => <li key={p.id} className="saved-place" data-lat={p.lat} data-lng={p.lng}>
 			<span className="saved-place__label">{p.name}</span>
 			<button className="ico ico--edit" onClick={editFavorite} value={p.id}>Edit</button>
-			<button className="ico ico--delete">Delete</button>
+			<button className="ico ico--delete" onClick={deleteFavorite} value={p.id}>Delete</button>
 		</li>)}
 	</ul>;
 }
