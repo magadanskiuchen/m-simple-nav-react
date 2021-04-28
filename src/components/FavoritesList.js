ca499c3a0854f1edad1e247a2cf06f8039e696d5
georgi <georgi@htmlbox.net>
2021-04-28 15:03:37 +0300

    Add ability to rename a favorite destination

--- src/components/FavoritesList.js
+++ src/components/FavoritesList.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import db from '../lib/db';
+
 import '../styles/FavoritesList.scss';
 
 export default function FavoritesList(props) {
+	const editFavorite = async (e) => {
+		const oldName = e.target.parentElement.querySelector('.saved-place__label').textContent
+		const newName = prompt("New Name", oldName);
+		
+		if (newName !== oldName) {
+			db.favorites.update(parseInt(e.target.value), { name: newName });
+		}
+	};
+	
 	return <ul className="favorites-list">
 		{props.favoritePlaces.map(p => <li key={p.id} className="saved-place" data-lat={p.lat} data-lng={p.lng}>
 			<span className="saved-place__label">{p.name}</span>
-			<button className="ico ico--edit">Edit</button>
+			<button className="ico ico--edit" onClick={editFavorite} value={p.id}>Edit</button>
 			<button className="ico ico--delete">Delete</button>
 		</li>)}
 	</ul>;
